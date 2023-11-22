bf86aaa84a86a918754d429ffb24a9d93f2bae76
Jxcka15 <124050158+Jxcka15@users.noreply.github.com>
2023-11-22 13:25:53 +0000

    Delete CW2/app directory

--- CW2/app/CW2/app/models.py
+++
@@ -1,7 +0,0 @@
-from extensions import db
-
-class leaderboard(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    score = db.Column(db.Integer)
-    date = db.Column(db.String(10))
